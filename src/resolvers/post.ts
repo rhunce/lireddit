78c3fc041a567d764e64b46ccaec8a86d23edd8a
Raymond Hunce <hunce18c@gmail.com>
2023-05-16 20:36:41 -0400

    Implement Post read resolver with id arg

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,19 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Ctx() { em }: MyContext,
+    @Arg("id", () => Int) id: number
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
   }
 }
