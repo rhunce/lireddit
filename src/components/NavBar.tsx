51398bdc1f97a671367cf8dc735bd55649a93635
Raymond Hunce <hunce18c@gmail.com>
2023-05-24 16:34:58 -0400

    Implement NavBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,46 @@
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
+import React from "react";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  let body;
+
+  console.log("DATA: ", data);
+
+  //   data is loading
+  if (fetching) {
+    body = null;
+
+    // user not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+
+    // user is logged in
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant={"link"}>Logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
+};
