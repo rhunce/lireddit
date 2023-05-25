530a31cf894a3517ecd89c06c83ca5be31c897b9
Raymond Hunce <hunce18c@gmail.com>
2023-05-25 19:26:17 -0400

    Comment in Urql Graphcache Cache Update implementation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,76 +1,70 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
 import { AppProps } from "next/app";
-import { Client, Provider, cacheExchange, fetchExchange } from "urql";
+import { Client, Provider, fetchExchange } from "urql";
+import { QueryInput, cacheExchange, Cache } from "@urql/exchange-graphcache";
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "../generated/graphql";
 
-// NOTE: Example of Urql Graphcache Cache Update feature (not needed at the moment)
-// import {
-//   QueryInput, cacheExchange, Cache,
-// } from "@urql/exchange-graphcache";
-// import {
-//   LoginMutation,
-//   MeDocument,
-//   MeQuery,
-//   RegisterMutation,
-// } from "../generated/graphql";
-
-// function betterUpdateQuery<Result, Query>(
-//   cache: Cache,
-//   qi: QueryInput,
-//   result: any,
-//   fn: (r: Result, q: Query) => Query
-// ) {
-//   return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
-// }
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = new Client({
   url: "http://localhost:3000/graphql",
   fetchOptions: {
     credentials: "include",
   },
-  exchanges: [cacheExchange, fetchExchange],
-  // NOTE: Example of Urql Graphcache Cache Update feature (not needed at the moment)
-  // exchanges: [
-  //   cacheExchange({
-  //     updates: {
-  //       Mutation: {
-  //         login: (res, _args, cache, _info) => {
-  //           betterUpdateQuery<LoginMutation, MeQuery>(
-  //             cache,
-  //             { query: MeDocument },
-  //             res,
-  //             (result, query) => {
-  //               if (result.login.errors) {
-  //                 return query;
-  //               } else {
-  //                 return {
-  //                   me: result.login.user,
-  //                 };
-  //               }
-  //             }
-  //           );
-  //         },
-  //         register: (res, _args, cache, _info) => {
-  //           betterUpdateQuery<RegisterMutation, MeQuery>(
-  //             cache,
-  //             { query: MeDocument },
-  //             res,
-  //             (result, query) => {
-  //               if (result.register.errors) {
-  //                 return query;
-  //               } else {
-  //                 return {
-  //                   me: result.register.user,
-  //                 };
-  //               }
-  //             }
-  //           );
-  //         },
-  //       },
-  //     },
-  //   }),
-  //   fetchExchange,
-  // ],
+  exchanges: [
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (res, _args, cache, _info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              res,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (res, _args, cache, _info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              res,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
