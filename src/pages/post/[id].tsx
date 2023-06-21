1c1f63ecc30fae4a4e00c880287a52d816f071e0
Raymond Hunce <hunce18c@gmail.com>
2023-06-20 20:34:27 -0400

    Update post detail page with EditDeletePostButtons component

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -3,8 +3,11 @@ import { createUrqlClient } from "../../utils/createUrqlClient";
 import { Layout } from "../../components/Layout";
 import { Box, Heading } from "@chakra-ui/react";
 import { useGetPostFromUrl } from "../../utils/useGetPostFromUrl";
+import { EditDeletePostButtons } from "../../components/EditDeletePostButtons";
+import { useMeQuery } from "../../generated/graphql";
 
 const Post = ({}) => {
+  const [{ data: meData }] = useMeQuery();
   const [{ data, fetching, error }] = useGetPostFromUrl();
 
   if (fetching) {
@@ -34,7 +37,10 @@ const Post = ({}) => {
   return (
     <Layout>
       <Heading mb={4}>{data.post.title}</Heading>
-      {data.post.text}
+      <Box mb={4}>{data.post.text}</Box>
+      {meData?.me?.id !== data.post.creator.id ? null : (
+        <EditDeletePostButtons id={data.post.id} />
+      )}
     </Layout>
   );
 };
