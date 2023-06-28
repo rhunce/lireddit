1e76ddc9753b68cc86216631543f8bd1cde21141
Raymond Hunce <hunce18c@gmail.com>
2023-06-28 18:40:51 -0400

    Add TODO to deploy.sh script

--- server/deploy.sh
+++ server/deploy.sh
@@ -5,5 +5,6 @@ read VERSION
 
 docker build -t rhunce/lireddit:$VERSION .
 docker push rhunce/lireddit:$VERSION
+# TODO: I believe `docker tag rhunce/lireddit:$VERSION dokku/api:latest` is useless here and can be removed. Test this.
 ssh root@159.223.98.194 "docker pull rhunce/lireddit:$VERSION && docker tag rhunce/lireddit:$VERSION dokku/api:latest && dokku git:from-image api rhunce/lireddit:$VERSION"
 
